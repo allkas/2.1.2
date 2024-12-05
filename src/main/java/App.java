fd914d9e383e624bc7dc048d5a46cefef3f7b002
Aleksey Karlov <all-kas@yandex.ru>
2024-12-05 22:37:20 +0300

    first commit

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -13,6 +13,7 @@ public class App {
 
         Cat cat = (Cat) applicationContext.getBean("cat");
         Cat cat1 = (Cat) applicationContext.getBean("cat");
+
         System.out.println(cat.equals(cat1));
     }
 }
